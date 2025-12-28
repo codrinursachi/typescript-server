3c53b377f42d57c237ecbea40e487f299b7af801
codrinursachi <codrinursachi@gmail.com>
2025-12-28 17:40:24 +0200

    feat: implement user management with PostgreSQL integration and environment configuration

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,5 @@
+process.loadEnvFile();
 export const config = {
     fileserverHits: 0,
+    dbURL: process.env.DB_URL || "",
 };
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
+process.loadEnvFile();
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 };
 export const config: APIConfig = {
     fileserverHits: 0,
+    dbURL: process.env.DB_URL || "",
 };
